9af893187e05bf5d66d3ed29f200c90542456338
Omar Hamad <omarh@duck.com>
2023-06-04 01:38:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 data = None  # This will store the last stored JSON data
@@ -8,14 +8,11 @@ def store_data():
     json_data = request.get_json()  # Get the JSON data from the request
     global data
     data = json_data  # Update the last stored JSON data
-    return 'Data stored successfully'
+    return jsonify({'message': 'Data stored successfully'})
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
-    if data is not None and 'data' in data:
-        return data['data'][0]  # Return the value at index 0
-    else:
-        return 'No data available'
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run()
