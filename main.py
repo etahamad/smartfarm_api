1178b9b5e60f6b262509fea625ab56f1170fa0d1
Omar Hamad <omarh@duck.com>
2023-06-07 13:02:09 +0300

    Update api

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -6,9 +7,16 @@ data = None  # This will store the last stored JSON data
 @app.route('/api/data', methods=['POST'])
 def store_data():
     json_data = request.get_json()  # Get the JSON data from the request
+    json_response = send_request_to_api(json_data)  # Send the request to the target API and get the JSON response
     global data
-    data = json_data  # Update the last stored JSON data
+    data = json_response  # Update the last stored JSON data
     return jsonify({'message': 'Data stored successfully'})
+
+def send_request_to_api(json_data):
+    url = 'https://etahamad-new-plant-disease-detection.hf.space/run/predict'
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(url, json=json_data, headers=headers, proxies={})  # Disable the proxy configuration
+    return response.json()  # Return the JSON response
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
