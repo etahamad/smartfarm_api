a37ab61e9daf1255480e02a903661c5191dd123e
Omar Hamad <omarh@duck.com>
2023-06-07 13:20:00 +0300

    default routes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 data = None  # This will store the last stored JSON data
 
-@app.route('/api/data', methods=['POST'])
+@app.route('/', methods=['POST'])
 def store_data():
     json_data = request.get_json()  # Get the JSON data from the request
     json_response = send_request_to_api(json_data)  # Send the request to the target API and get the JSON response
@@ -18,7 +18,7 @@ def send_request_to_api(json_data):
     response = requests.post(url, json=json_data, headers=headers, proxies={})  # Disable the proxy configuration
     return response.json()  # Return the JSON response
 
-@app.route('/api/data', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_data():
     return jsonify(data)
 
